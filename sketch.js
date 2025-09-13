10788c389c28b96a50db6a49d495663c0d84cbac
Nadia <nadia80@gmx.ch>
2025-09-13 15:06:37 +0200

    Nadias edit

--- sketch.js
+++ sketch.js
@@ -15,30 +15,30 @@ let Engine, World, Bodies, Body;
 let engine, world;
 let ellipses = [];
 let ground;
-let spawnInterval = 20;
+let spawnInterval = 5; // spawn more frequently
 let lastSpawnFrame = 0;
 
 let waitingEllipses = [];
 
 function spawnWaitingEllipse() {
-  // Spawn at bottom of pgW, random x
-  let radius = random(4, 10);
+  // Spawn at top of pgW, random x
+  let radius = random(10, 30); // bigger ellipses
   let x = random(radius, pgW.width - radius);
-  let y = pgW.height - radius;
+  let y = radius;
   let col = [random(255), random(255), random(255), 180];
-  waitingEllipses.push({ x, y, radius, col, phase: 'pgW' });
+  waitingEllipses.push({ x, y, radius, col });
 }
 
 function transferEllipseToPg1(waitingEllipse) {
-  // Start transition animation from top of pgW to bottom of pg1
+  // Start transition animation from bottom of pgW to top of pg1
   waitingEllipse.phase = 'transition';
   waitingEllipse.t = 0; // progress for animation
 }
 
 function finalizeEllipseInPg1(waitingEllipse) {
-  // Map x from pgW to pg1, y to bottom of pg1
-  let mappedX = map(waitingEllipse.x, 0, pgW.width, 0, pg1.width);
-  let mappedY = pg1.height - waitingEllipse.radius;
+  // Always enter at upper left corner of pg1
+  let mappedX = waitingEllipse.radius;
+  let mappedY = waitingEllipse.radius;
   let body = Bodies.circle(mappedX, mappedY, waitingEllipse.radius, {
     restitution: 0.5,
     friction: 0.1,
@@ -91,41 +91,23 @@ function draw() {
   // Physics update
   Engine.update(engine);
 
-  // Spawn new waiting ellipses at bottom of pgW
+  // Spawn new waiting ellipses at top of pgW
   if (frameCount - lastSpawnFrame > spawnInterval) {
     spawnWaitingEllipse();
     lastSpawnFrame = frameCount;
   }
 
-  // Animate waiting ellipses
+  // Animate waiting ellipses moving down through pgW
   pgW.noStroke();
   for (let i = waitingEllipses.length - 1; i >= 0; i--) {
     let e = waitingEllipses[i];
-    if (e.phase === 'pgW') {
-      // Move up through pgW
-      pgW.fill(...e.col);
-      pgW.ellipse(e.x, e.y, e.radius * 2, e.radius * 2);
-      e.y -= 2;
-      if (e.y <= e.radius) {
-        // Start transition to pg1
-        transferEllipseToPg1(e);
-      }
-    } else if (e.phase === 'transition') {
-      // Animate from top of pgW to bottom of pg1
-      // Map x from pgW to pg1
-      let mappedX = map(e.x, 0, pgW.width, 0, pg1.width);
-      let startY = e.radius;
-      let endY = pg1.height - e.radius;
-      // Animate t from 0 to 1
-      e.t += 0.05;
-      let currentY = lerp(startY, endY, e.t);
-      pg1.fill(...e.col);
-      pg1.ellipse(mappedX, currentY, e.radius * 2, e.radius * 2);
-      if (e.t >= 1) {
-        // Finalize in pg1 with physics
-        finalizeEllipseInPg1(e);
-        waitingEllipses.splice(i, 1);
-      }
+    pgW.fill(...e.col);
+    pgW.ellipse(e.x, e.y, e.radius * 2, e.radius * 2);
+    e.y += 2;
+    if (e.y >= pgW.height - e.radius) {
+      // Enter pg1 at upper left corner and fall down
+      finalizeEllipseInPg1(e);
+      waitingEllipses.splice(i, 1);
     }
   }
 
